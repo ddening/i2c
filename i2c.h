a80f498e7c179e76c995e50dec5bdab90fc20ae0
ddening <44156662+ddening@users.noreply.github.com>
2023-03-28 16:52:23 +0200

    Create i2c device definition

--- i2c.h
+++ i2c.h
@@ -40,12 +40,22 @@
 #include "i2c_io.h"
 #include "i2c_config.h"
 #include "i2c_error_handler.h"
-#include "../ringbuffer/ringbuffer.h"
+#include "ringbuffer.h"
+
+/* Describes a i2c device */
+typedef struct device_t {
+    uint8_t address;
+    uint8_t pin;
+    uint8_t port;
+    uint8_t ddr;
+} device_t;
 
 i2c_error_t i2c_init(i2c_config_t*);
 
 i2c_error_t i2c_read(void);
 
 i2c_error_t i2c_write(payload_t*);
+
+extern payload_t* payload_create_i2c(priority_t priority, device_t* device, uint8_t* data, uint8_t number_of_bytes, callback_fn* callback);
 
 #endif /* I2C_H_ */
