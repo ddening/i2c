ea4b837c1a3742ce03abf9cdca6b397335af119a
ddening <44156662+ddening@users.noreply.github.com>
2025-03-06 16:14:07 +0100

    Init

---
+++ test_i2c/test_i2c.h
@@ -0,0 +1,37 @@
+/*************************************************************************
+* Title		: I2C Unit Test
+* Author	: Dimitri Dening
+* Created	: 13.02.2022 19:30:22
+* Software	: Microchip Studio V7
+* Hardware	: Atmega2560
+* Usage		: see Doxygen manual
+*
+* This is a modified version of Microchip Studios' SPI Unit Test.
+* Following the license from Microchip used in the original file.
+*
+* Subject to your compliance with these terms, you may use Microchip
+* software and any derivatives exclusively with Microchip products.
+* It is your responsibility to comply with third party license terms applicable
+* to your use of third party software (including open source software) that
+* may accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
+* WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+* INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+* AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+* LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+* LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+* SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+* POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+* ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+* RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*
+*************************************************************************/
+
+#ifndef TEST_I2C_H_
+#define TEST_I2C_H_
+
+int test_i2c(void);
+
+#endif /* TEST_I2C_H_ */
