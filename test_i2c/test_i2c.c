a608e030c4fd21a62f244ae11e87af11eff9f1c0
ddening <44156662+ddening@users.noreply.github.com>
2025-03-14 18:58:20 +0100

    test: Remove unused test case

--- test_i2c/test_i2c.c
+++ test_i2c/test_i2c.c
@@ -49,35 +49,9 @@
 #include "uart.h"
 #include "led_lib.h"
 #include "heartbeat.h"
+#include "memory.h"
 	
 #define I2C_DEVICE_ADDR 0x27  
-
-/* HD44780 */
-#define LINE_COUNT  0x04
-#define LINE_LENGTH 0x14
-#define LINE1	0x00
-#define LINE2	LINE1 + 0x40
-#define LINE3	LINE1 + LINE_LENGTH
-#define	LINE4 	LINE2 + LINE_LENGTH
-#define DDRAM_ADDR 0x80
-
-/* OPCODE */
-#define RSRW00 0x00
-#define RSRW01 0x01
-#define RSRW10 0x02
-#define RSRW11 0x03
-
-#define WRITE_TEST_CHAR 0x48
-#define ALL_ON          0x0F
-#define DISPLAY_OFF     0x08
-#define DISPLAY_ON      0x0C
-#define CURSOR_ON       0x0A 
-#define BLINK_ON        0x09   
-#define CLEAR_DISPLAY   0x01
-#define RETURN_HOME     0x02
-#define ENTRY_MODE      0x07
-#define FUNCTION_SET_EUROPEAN 0x3C
-#define FUNCTION_SET_4_BIT_MODE 0x28 // 4 Bit Mode, 2 Lines, 5x8 Dots, 0x20 := Only 4 Bit Set
 
 /* Callback Flags */
 static bool memory_return_success = 0;
@@ -90,30 +64,13 @@ static device_t* i2c_device;
 static uint8_t dummy_payload[] = { 1, 2, 3, 4, 5 };
 
 static int run_i2c_payload_test(const struct test_case* test){
+       
+    payload_t* payload = payload_create_i2c(PRIORITY_NORMAL, i2c_device, dummy_payload, ARRAY_LEN(dummy_payload), NULL);
     
-    // i2c_device = i2c_create_device(I2C_DEVICE_ADDR);
-    
-    if (i2c_device == NULL) {
-        return TEST_ERROR;
-    }
-    
-    uint8_t* data = (uint8_t*)malloc(sizeof(uint8_t) * ARRAY_LEN(dummy_payload));
-    
-    if (data == NULL) {
-        return TEST_ERROR;
-    }
-	
-	for (uint8_t i = 0; i < ARRAY_LEN(dummy_payload); i++) {
-		data[i] = dummy_payload[i];
-	}
-    
-    payload_t* payload = payload_create_i2c(PRIORITY_NORMAL, i2c_device, data, ARRAY_LEN(dummy_payload), NULL);
-    
-    if (payload->priority != PRIORITY_NORMAL                                ||
-        payload->protocol.i2c.device->address != I2C_DEVICE_ADDR            ||
-        payload->protocol.i2c.number_of_bytes != ARRAY_LEN(dummy_payload)   ||
-        payload->protocol.i2c.callback != NULL) {
-            free(data);
+    if (payload->priority != PRIORITY_NORMAL                       ||
+        payload->i2c.device->address != I2C_DEVICE_ADDR            ||
+        payload->i2c.number_of_bytes != ARRAY_LEN(dummy_payload)   ||
+        payload->i2c.callback != NULL) {
             free(payload);
             free(i2c_device);
             return TEST_FAIL;
@@ -121,8 +78,7 @@ static int run_i2c_payload_test(const struct test_case* test){
         
     /* Check data content */
     for (uint8_t i = 0; i < ARRAY_LEN(dummy_payload); i++) {
-        if (payload->protocol.i2c.data[i] != dummy_payload[i]){
-            free(data);
+        if (payload->i2c.data[i] != dummy_payload[i]){
             free(payload);
             free(i2c_device);
             return TEST_FAIL;
@@ -130,74 +86,27 @@ static int run_i2c_payload_test(const struct test_case* test){
     }
 
 	i2c_write(payload);
-    
-    //free(data);
-    //free(payload);
-    //free(i2c_device);  
 	
     return TEST_PASS;
 }
 
-static int run_i2c_memory_leak_test(const struct test_case* test){
-    
-	uint32_t number_of_tasks;
-	payload_t* payload;
-	
-	number_of_tasks = 30000;
-	for (int i = 0; i < number_of_tasks; i++) {
-		
-		payload = payload_create_i2c(PRIORITY_NORMAL, i2c_device, dummy_payload, ARRAY_LEN(dummy_payload), &callback_memory_leak);
-		
-		if (payload == NULL) {
-			return TEST_ERROR;
-		}
-		
-		i2c_write(payload);
-		
-		while(memory_return_success != 1);
-		
-		memory_return_success = 0;
-	}
-
-	return TEST_PASS;
-}
-
-static void _check_fake_busy (void) {
-	_delay_ms(20); // Fake Busy Response
-}
-
-static int run_i2c_to_hd44780_test(const struct test_case* test){
-	
-	// TODO: use hd44780_i2c here
-	
-    return TEST_PASS;
-}
-  
 void test_i2c(void) {
     
 	cli();		
 	
-	led_init();
+	uart_init();
 	
-	heartbeat_init();
-	
-	//uart_init();
-	
-	i2c_init(); // &i2c_config
+	i2c_init();
     
     i2c_device = i2c_create_device(I2C_DEVICE_ADDR);
 	
 	sei();	
   	
 	DEFINE_TEST_CASE(i2c_payload_test, NULL, run_i2c_payload_test, NULL, "I2C payload test");
-	DEFINE_TEST_CASE(i2c_memory_leak_test, NULL, run_i2c_memory_leak_test, NULL, "I2C memory leak test");
-	DEFINE_TEST_CASE(i2c_to_hd44780_test, NULL, run_i2c_to_hd44780_test, NULL, "I2C to HD44780 test");
 
 	/* Put test case addresses in an array */
 	DEFINE_TEST_ARRAY(i2c_tests) = {
-		//&i2c_payload_test,
-        //&i2c_memory_leak_test,
-		//&i2c_to_hd44780_test,
+		&i2c_payload_test,
 	};
 	
 	/* Define the test suite */
@@ -205,4 +114,8 @@ void test_i2c(void) {
     
 	/* Run all tests in the test suite */
 	test_i2c_suite_run(&i2c_suite);
+	
+	free(i2c_device);
+	
+	// memory_report();
 }
